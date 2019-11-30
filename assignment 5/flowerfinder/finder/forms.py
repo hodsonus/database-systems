2e959ab1fc4e8a43a939c0a4322beb1e81ef211b
John Hodson <hodsonus@gmail.com>
2019-11-30 13:24:14 -0500

    finished rough project implementation

--- assignment 5/flowerfinder/finder/forms.py
+++ assignment 5/flowerfinder/finder/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from .models import Flowers
 
-class FlowerForm(forms.Form):
+class FlowerSelectForm(forms.Form):
     flowers = forms.ChoiceField(choices=[(flower.comname, flower.comname) for flower in Flowers.objects.raw('SELECT * FROM flowers where comname like "%"')], \
-                                widget=forms.Select(attrs={'onchange': 'fillTableOnChange(this.options[this.selectedIndex].value);'}))
+                                widget=forms.Select(attrs={'onchange': 'onSelectFormChange(this.options[this.selectedIndex].value);'}))
 
-    def get(self):
-        print('FlowerForm: I made it !')
+class FlowerInformationForm(forms.Form):
+    genus = forms.CharField(label='Genus', required=True, max_length=30)
+    species = forms.CharField(label='Species', required=True, max_length=30)
+
+class NewSightingForm(forms.Form):
+    person = forms.CharField(label='Person', required=True, max_length=30)
+    location = forms.CharField(label='Location', required=True, max_length=30)
+    sighted = forms.DateField(label='Sighted', input_formats=['%d-%m-%Y'], required=True)
--- assignment 5/flowerfinder/finder/views.py
+++ assignment 5/flowerfinder/finder/views.py
@@ -1,20 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sightings, Features, Flowers, Members
-from .forms import FlowerForm
+from .forms import FlowerSelectForm, FlowerInformationForm, NewSightingForm
 from django.http import JsonResponse
 from django.core.serializers import serialize
+from django.db import connection
 
 def index(request):
     return render(request, 'index.html')
 
 def flowers(request):
-    return render(request, 'flowers.html')
+    if request.method == 'GET':
+        path = request.path.strip('/').split('/')
+        if (len(path) == 1):
+            return render(request, 'flowers.html', {'flower_select_form': FlowerSelectForm(), 'flower_information_form': FlowerInformationForm()})
+        else:
+            flower_comname = path[1].replace('_', ' ')
+            query = 'SELECT * FROM flowers where comname like "{}" limit 1'.format(flower_comname)
+            flowers = Flowers.objects.raw(query)
+
+            data = []
+            for flower in flowers:
+                data += [
+                            { 
+                                'genus' : flower.genus,
+                                'species' : flower.species,
+                                'comname' : flower.comname,
+                            } 
+                        ]
+            return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        comname = request.POST.get("flowers", "")
+        new_genus = request.POST.get("genus", "")
+        new_species = request.POST.get("species", "")
+
+        cursor = connection.cursor()
+        query = "UPDATE flowers SET genus = \"{}\", species=\"{}\" WHERE comname=\"{}\"".format(new_genus, new_species, comname)
+        cursor.execute(query)
+
+        return render(request, 'flowers.html', {'flower_select_form': FlowerSelectForm(), 'flower_information_form': FlowerInformationForm()})
 
 def sightings(request):
     path = request.path.strip('/').split('/')
     if (len(path) == 1):
-        return render(request, 'sightings.html', {'form': FlowerForm()})
+        return render(request, 'sightings.html', {'flower_select_form': FlowerSelectForm()})
     else:
         flower_comname = path[1].replace('_', ' ')
         query = 'SELECT * FROM sightings where name like "{}" order by sighted desc limit 10'.format(flower_comname)
@@ -28,10 +57,20 @@ def sightings(request):
                             'sighted' : sighting.sighted.strftime("%m/%d/%Y")
                         } 
                     ]
-        # resp = serialize('json', sightings)
-        # print(resp)
-        # return HttpResponse(resp)
         return JsonResponse(data, safe=False)
 
 def log(request):
-    return render(request, 'log.html')
+    if request.method == 'GET':
+        return render(request, 'log.html', {'flower_select_form': FlowerSelectForm(), 'new_sighting_form': NewSightingForm()})
+    elif request.method == 'POST':
+        name = request.POST.get("flowers", "")
+        person = request.POST.get("person", "")
+        location = request.POST.get("location", "")
+        sighted = request.POST.get("sighted", "")
+
+        cursor = connection.cursor()
+        query = "INSERT INTO SIGHTINGS VALUES (\"{}\", \"{}\", \"{}\", \"{}\")".format(name, person, location, sighted)
+        # print(query)
+        cursor.execute(query)
+
+        return render(request, 'log.html', {'flower_select_form': FlowerSelectForm(), 'new_sighting_form': NewSightingForm()})
