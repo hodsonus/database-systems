8e0c315343ae9685823778893b9ee71c25e46684
John Hodson <hodsonus@gmail.com>
2019-12-06 12:10:52 -0500

    finish assignment 4 submission

--- assignment 4/unkp-task1.sql
+++ assignment 4/unkp-task1.sql
@@ -1,9 +1,8 @@
 CREATE TRIGGER UNKPTASK1
-BEFORE INSERT ON SIGHTINGS
-FOR EACH ROW
-BEGIN
-    SELECT CASE
-        WHEN NEW.PERSON NOT IN (SELECT NAME FROM MEMBERS)
-            THEN RAISE(ABORT, 'Error: Insert into the SIGHTINGS table references a person that is not found in the database.')
+    AFTER INSERT ON SIGHTINGS
+    FOR EACH ROW
+    WHEN NEW.PERSON NOT IN (SELECT NAME FROM MEMBERS)
+    BEGIN
+        SELECT RAISE(IGNORE);
+        -- print 'Error: Insert into the SIGHTINGS table references a person that is not found in the database.'
     END;
-END;
